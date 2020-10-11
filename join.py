c5758cc87d35e6dad52bda8ac68fce7ea26b8433
surveyorjr <matingonk@gmail.com>
2020-10-11 12:54:45 +0200

    quick fixes on code

--- join.py
+++ join.py
@@ -1,6 +1,6 @@
 import math
 
-print('WELCOME TO MY JOIN PRORAM')
+print('WELCOME TO MY JOIN PROGRAM')
 print('STARTING POINT \n\n Input Y-Coordinate')
 
 coord_a_y = float(input())
@@ -15,26 +15,26 @@ dif_y = coord_b_y - coord_a_y
 dif_x = coord_b_x - coord_a_x
 
 distance = math.sqrt((pow(dif_x, 2) + pow(dif_y, 2)))
-print("***********RESULT**************\n\nDistance => "+str(distance))
+print("***********RESULT**************\n\nDistance => " + str(distance))
 
 # finding the angle now
-rad_theta = math.atan(dif_y/dif_x)
+rad_theta = math.atan(dif_y / dif_x)
 
 # declaring a constant rad value to convert from radians to degrees
-rad_constant = 180/math.pi
+rad_constant = 180 / math.pi
 
-# conditions to find qudrants
+# conditions to find quadrants
 
-#second quadrant
-if (dif_x < 0 and dif_y > 0):
+# second quadrant
+if dif_x < 0 < dif_y:
     quadrant = 180
 
-#third quadrant
-elif (dif_x < 0 and dif_y < 0):
+# third quadrant
+elif dif_x < 0 and dif_y < 0:
     quadrant = 180
 
-#fourth quadrant
-elif (dif_x > 0 and dif_y < 0):
+# fourth quadrant
+elif dif_x > 0 > dif_y:
     quadrant = 360
 
 # when neither condition is met and its first quadrant
@@ -49,4 +49,4 @@ minutes = int(minutes_float)
 seconds = math.floor((minutes_float - minutes) * 60)
 
 # the angle in DMS format
-print("Angle => "+str(degree)+"°"+str(minutes)+"`"+str(seconds)+"``")
+print("Angle => " + str(degree) + "°" + str(minutes) + "`" + str(seconds) + "``")
